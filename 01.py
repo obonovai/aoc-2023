fd27ca4133b52aeee2c3da8dc7179746b98e26d2
Ivona Oboňová <ivonaobonova@gmail.com>
2023-12-02 11:14:22 +0100

    refactor: change array and sum for simple int

--- 01.py
+++ 01.py
@@ -1,11 +1,11 @@
 def solve(numbers: dict[str, int]) -> int:
-    results = []
+    result = 0
     with open('01/input') as f:
         for line in f:
             lvalues = {line.find(key): value for key, value in numbers.items() if line.find(key) > -1}
             rvalues = {line.rfind(key): value for key, value in numbers.items() if line.rfind(key) > -1}
-            results += [int(f'{lvalues[min(lvalues)]}{rvalues[max(rvalues)]}')]
-    return sum(results)
+            result += int(f'{lvalues[min(lvalues)]}{rvalues[max(rvalues)]}')
+    return result
 
 numbers = {'1': 1, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9}
 print(solve(numbers))
